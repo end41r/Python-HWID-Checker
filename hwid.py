19ce15d16d0d7a73c86deefc6f11f8d80c469ccb
end41r <82461251+end41r@users.noreply.github.com>
2021-07-14 17:17:26 +0200

    Update hwid.py
    resized till 7 strings and deleted unused variables

--- hwid.py
+++ hwid.py
@@ -1,18 +1,7 @@
-import getpass
-import os
+import getpass, os, uuid, hashlib
 from getmac import get_mac_address as gma
-import uuid
-import hashlib
 from requests import get
-
-hwid = os.name + getpass.getuser() + gma() + str(hex(uuid.getnode()))
-
 file = get(url='raw pastebin with hwids goes here')
-stripped = file.text[:500]
-stripped = stripped.split('\r\n')
-
-x = len(stripped)
-
-for i in range(x):
-    if hashlib.sha256(hwid.encode()).hexdigest() == stripped[i]:
-        print("ok")
+stripped = file.text[:500].split('\r\n')
+for i in range(len(stripped)):
+    if hashlib.sha256((os.name + getpass.getuser() + gma() + str(hex(uuid.getnode()))).encode()).hexdigest() == stripped[i]: print("ok")
