63f679e418573886236e16a6e647ce5b56eb14c3
end41r <82461251+end41r@users.noreply.github.com>
2021-07-16 21:50:30 +0200

    Update hwid.py
    some bugfix

--- hwid.py
+++ hwid.py
@@ -1,5 +1,5 @@
 import getpass, os, uuid, hashlib, getmac as gma, requests
-file = get(url='raw pastebin with hwids goes here')
+file = requests.get(url='raw pastebin with hwids goes here')
 stripped = file.text[:500].split('\r\n')
 for i in range(len(stripped)):
-    if hashlib.sha256((os.name + getpass.getuser() + gma() + str(hex(uuid.getnode()))).encode()).hexdigest() == stripped[i]: print("ok")
+    if hashlib.sha256((os.name + getpass.getuser() + gma.get_mac_address() + str(hex(uuid.getnode()))).encode()).hexdigest() == stripped[i]: print("ok")
